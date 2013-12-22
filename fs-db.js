8b367744e6f35c3c367ebd7cd5d98a23f1413079
Ben West <bewest@gmail.com>
2013-12-21 22:36:06 -0800

    clean up tests, emit no debug output
    Fix up tests to run reliably.
    According to the spec for update-pack, refs are supposed to be sorted, anyway.

--- fs-db.js
+++ fs-db.js
@@ -90,8 +90,8 @@ module.exports = function (platform) {
       if (!callback) return keys.bind(this, prefix);
       var results = [ ];
       var targets = [ ];
+
       function listKeys (prefix, cb) {
-        console.log('listKeys', prefix);
         var list = [ ];
         fs.readdir(prefix, function (err, found) {
           if (!err) found.forEach(function (item) {
@@ -100,64 +100,49 @@ module.exports = function (platform) {
             list.push(name);
           });
           cb(err, list);
-
         });
       }
+
       function shift ( ) {
         var t = targets.shift( );
-        console.log("LEFT", targets.length);
         if (!t) {
           finish(null, null);
         }
-
       }
-      function onKeys (err, list) {
-        function isKeyName (name, cb) {
-          listKeys(name, function (err, found) {
-            if (err && (err.code == 'ENOENT' || err.code == 'ENOTDIR')) {
-              err = null;
-              // shift( );
-              return cb(null, name);
-            }
-            cb(err, null, found);
-          });
-        }
-        console.log("err", err);
-        console.log("LIST", targets.length, list);
-        if (!err) list.forEach(function (key, i) {
-            // targets.unshift(key);
 
-            console.log('forEach list', 'err', err, 'key', key);
+      function isKeyName (name, cb) {
+        listKeys(name, function (err, found) {
+          if (err && (err.code == 'ENOENT' || err.code == 'ENOTDIR')) {
+            err = null;
+            return cb(null, name);
+          }
+          cb(err, null, found);
+        });
+      }
+
+      function onKeys (err, list) {
+        if (!err) list.sort( ).forEach(function (key, i) {
             var name = key;
-            console.log('name', name);
             isKeyName(name, function (err, found, remain) {
-              console.log ('FOUND', i, '(', name ,')', err, found, remain ? remain.length : remain, 'left');
-              var count = i;
               shift( );
               if (remain) return onKeys(err, remain);
               if (name) results.push(name);
-              if (!err && !remain && count == list.length - 1) {
-                console.log("END?", 'targets', targets.length, 'list', list.length, list, 'count', count);
+              if (!err && !remain && i == list.length - 1) {
                 if (targets.length === 0) {
                   shift( );
                 }
               }
             });
         });
-        if (!err) {
-
-        }
-
       }
+
       function finish (err, data) {
-        console.log("FINISH", targets.length);
         if (err) callback(err);
         if (data) onKeys(err, data.slice(0));
         if (!data) callback(err, results);
       }
 
-      return listKeys(prefix || "/", onKeys);
-      // return fs.readdir(prefix || "/", callback);
+      return listKeys(prefix || "/", finish);
     }
 
 
--- test/test.js
+++ test/test.js
@@ -11,10 +11,14 @@ if (!module.parent) {
   function msg (msg) {
     return function ( ) { console.log(msg, arguments); };
   }
+  function match(ref) {
+    db.keys(ref, msg('MATCH ' + ref));
+  }
   console.log('db', db);
   // db.keys('refs/', msg("XXX MAIN REFS"));
   // db.keys('refs/foo/qux', msg("XXXX refs/foo/qux"));
-  db.keys('refs/foo/', msg("XXX refs/foo/bar"));
+  match('refs/foo/');
+  match('refs/two');
 
 } else {
   describe('git-fs-db backend', function ( ) {
@@ -49,9 +53,9 @@ if (!module.parent) {
         var path = platform.fs('test/keys');
         var db = fsDB(path);
         should.exist(db.init);
-        db.init( );
 
         it('should generate one match given refs/one', function (done) {
+          db.init( );
           db.keys('refs/one', function (err, keys) {
             should.not.exist(err);
             var correct = [ 'refs/one/.gitkeep' ];
@@ -64,6 +68,7 @@ if (!module.parent) {
         });
 
         it('should generate matches given refs/two', function (done) {
+          db.init( );
           db.keys('refs/two', function (err, keys) {
             should.not.exist(err);
             var correct = [ 'refs/two/aaa', 'refs/two/bbb' ];
@@ -76,17 +81,13 @@ if (!module.parent) {
         });
 
         it('should generate matches given refs/three', function (done) {
+          db.init( );
           db.keys('refs/three', function (err, keys) {
             should.not.exist(err);
-            var correct = [ 'refs/three/two/aaa',
-                            'refs/three/two/ccc',
-                            'refs/three/two/bbb',
-                            'refs/three/one/aaa',
-                            'refs/three/one/ccc',
-                            'refs/three/one/bbb',
-                            'refs/three/three/aaa',
-                            'refs/three/three/ccc',
-                            'refs/three/three/bbb' ];
+            var correct =  [ 'refs/three/one/aaa', 'refs/three/one/bbb', 'refs/three/one/ccc',
+                'refs/three/three/aaa', 'refs/three/three/bbb', 'refs/three/three/ccc',
+                'refs/three/two/aaa', 'refs/three/two/bbb', 'refs/three/two/ccc' ]
+              ;
 
             keys.length.should.equal(9);
 
@@ -97,25 +98,27 @@ if (!module.parent) {
         });
 
         it('should generate matches given refs/four', function (done) {
+          db.init( );
           db.keys('refs/four', function (err, keys) {
             should.not.exist(err);
-            var correct = [ 'refs/four/two/cc/ccc', 'refs/four/two/cc/ddd',
-                'refs/four/two/cc/bbb', 'refs/four/two/cc/aaaa', 'refs/four/two/aa/ccc',
-                'refs/four/two/aa/ddd', 'refs/four/two/aa/bbb', 'refs/four/two/aa/aaaa',
-                'refs/four/two/bb/ccc', 'refs/four/two/bb/ddd', 'refs/four/two/bb/bbb',
-                'refs/four/two/bb/aaaa', 'refs/four/two/dd/ccc', 'refs/four/two/dd/ddd',
-                'refs/four/two/dd/bbb', 'refs/four/two/dd/aaaa', 'refs/four/one/cc/ccc',
-                'refs/four/one/cc/ddd', 'refs/four/one/cc/bbb', 'refs/four/one/cc/aaaa',
-                'refs/four/one/aa/ccc', 'refs/four/one/aa/ddd', 'refs/four/one/aa/bbb',
-                'refs/four/one/aa/aaaa', 'refs/four/one/bb/ccc', 'refs/four/one/bb/ddd',
-                'refs/four/one/bb/bbb', 'refs/four/one/bb/aaaa', 'refs/four/one/dd/ccc',
-                'refs/four/one/dd/ddd', 'refs/four/one/dd/bbb', 'refs/four/one/dd/aaaa',
-                'refs/four/three/cc/ccc', 'refs/four/three/cc/ddd', 'refs/four/three/cc/bbb',
-                'refs/four/three/cc/aaaa', 'refs/four/three/aa/ccc', 'refs/four/three/aa/ddd',
-                'refs/four/three/aa/bbb', 'refs/four/three/aa/aaaa', 'refs/four/three/bb/ccc',
-                'refs/four/three/bb/ddd', 'refs/four/three/bb/bbb', 'refs/four/three/bb/aaaa',
-                'refs/four/three/dd/ccc', 'refs/four/three/dd/ddd', 'refs/four/three/dd/bbb',
-                'refs/four/three/dd/aaaa' ]
+            var correct = [ 'refs/four/one/aa/aaaa', 'refs/four/one/aa/bbb',
+                'refs/four/one/aa/ccc', 'refs/four/one/aa/ddd', 'refs/four/one/bb/aaaa',
+                'refs/four/one/bb/bbb', 'refs/four/one/bb/ccc', 'refs/four/one/bb/ddd',
+                'refs/four/one/cc/aaaa', 'refs/four/one/cc/bbb', 'refs/four/one/cc/ccc',
+                'refs/four/one/cc/ddd', 'refs/four/one/dd/aaaa', 'refs/four/one/dd/bbb',
+                'refs/four/one/dd/ccc', 'refs/four/one/dd/ddd', 'refs/four/three/aa/aaaa',
+                'refs/four/three/aa/bbb', 'refs/four/three/aa/ccc', 'refs/four/three/aa/ddd',
+                'refs/four/three/bb/aaaa', 'refs/four/three/bb/bbb', 'refs/four/three/bb/ccc',
+                'refs/four/three/bb/ddd', 'refs/four/three/cc/aaaa', 'refs/four/three/cc/bbb',
+                'refs/four/three/cc/ccc', 'refs/four/three/cc/ddd', 'refs/four/three/dd/aaaa',
+                'refs/four/three/dd/bbb', 'refs/four/three/dd/ccc', 'refs/four/three/dd/ddd',
+                'refs/four/two/aa/aaaa', 'refs/four/two/aa/bbb', 'refs/four/two/aa/ccc',
+                'refs/four/two/aa/ddd', 'refs/four/two/bb/aaaa', 'refs/four/two/bb/bbb',
+                'refs/four/two/bb/ccc', 'refs/four/two/bb/ddd', 'refs/four/two/cc/aaaa',
+                'refs/four/two/cc/bbb', 'refs/four/two/cc/ccc', 'refs/four/two/cc/ddd',
+                'refs/four/two/dd/aaaa', 'refs/four/two/dd/bbb', 'refs/four/two/dd/ccc',
+                'refs/four/two/dd/ddd' ]
+
             ;
             keys.length.should.equal(48);
             text(keys).should.equal(text(correct));
@@ -125,14 +128,14 @@ if (!module.parent) {
         });
 
         it('should generate matches given refs/foo', function (done) {
+          db.init( );
           db.keys('refs/foo', function (err, keys) {
             should.not.exist(err);
             var correct = [ 'refs/foo/qan/.gitkeep', 'refs/foo/qux/.gitkeep',
-                'refs/foo/bar/bee/.gitkeep', 'refs/foo/bar/buzz/.gitkeep',
-                'refs/foo/bar/bazz/.gitkeep' ]
+                'refs/foo/bar/bazz/.gitkeep', 'refs/foo/bar/bee/.gitkeep',
+                'refs/foo/bar/buzz/.gitkeep' ]
             ;
             keys.length.should.equal(5);
-
             text(keys).should.equal(text(correct));
             done( );
           });
